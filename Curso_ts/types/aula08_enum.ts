9f84a8fea6d6d7e4484e5785e35bcb114ab923e6
luk3rF7 <lkzeron@gmail.com>
2023-07-04 18:16:28 -0300

    enumerar tipos

---
+++ Curso_ts/types/aula08_enum.ts
@@ -0,0 +1,23 @@
+//como criar enum
+
+enum Cors {
+  //adiciono os campos:
+  VERMELHO = 100, //* posso usar = 1 dai ele começa
+  BLUE = 200,
+  GREEN = "GREEN",
+  YELLO = 201,
+}
+
+enum Cors {
+  PINK = 205,
+  PURPLE = "PURPLE",
+  GREY = 208,
+}
+
+function optionCors(cor: Cors): void {
+  console.log(Cors[cor]);
+}
+
+optionCors(Cors.PURPLE);
+console.log(Cors);
+console.log(Cors);
